e251faecd099ae4989e905033a4953c34c61732b
tropicalwave <44818573+tropicalwave@users.noreply.github.com>
2022-07-25 23:00:20 +0200

    temporarily downgrade Keycloak version due to build issues

--- keycloak/ci/startup.sh
+++ keycloak/ci/startup.sh
@@ -2,4 +2,4 @@
 set -e
 
 sed "s/\$KEYCLOAK_JENKINS_SECRET/$KEYCLOAK_JENKINS_SECRET/;s/\$KEYCLOAK_GITEA_SECRET/$KEYCLOAK_GITEA_SECRET/;s/\$LDAP_BIND_CREDENTIAL/$LDAP_BIND_CREDENTIAL/" /opt/ci/ci-realm.json >/tmp/ci-realm.json
-exec /opt/keycloak/bin/kc.sh
+exec /opt/jboss/tools/docker-entrypoint.sh -b 0.0.0.0
